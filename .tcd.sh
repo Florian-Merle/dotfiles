f34a7886537b32a6b85b51d07a9f4453fee5bb7b
Florian Merle <fmerle@consultant.afflelou.net>
2022-08-03 23:12:26 +0200

    add tcd script for tmux

---
+++ .tcd.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# Tmux Create Dashboard
+
+path=${1:-$(pwd)}
+basename=$(basename $path)
+
+# Check if session exist
+found=$(tmux list-session -F '#{session_name}' | grep -Fx $basename)
+
+if [ -n "$found" ]
+then
+	tmux attach-session -d -t $basename -c $path
+	exit 0
+fi
+
+# Create session
+tmux new-session -d -s $basename 
+
+# Create panes
+tmux split-window -v -p 25 'git logor'
+tmux split-window -h -p 25 'git brr'
+
+# Finaly, attach to the session
+tmux attach-session -d -t $basename -c $path
