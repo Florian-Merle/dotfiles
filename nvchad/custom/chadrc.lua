4a4ad5508b5426d2126dc67c19cd395de727726e
Florian Merle <florian.david.merle@gmail.com>
2022-09-05 18:24:49 +0200

    Cleanup comments

--- nvchad/custom/chadrc.lua
+++ nvchad/custom/chadrc.lua
@@ -2,12 +2,7 @@ local override = require 'custom.plugins.override'
 local mappings = require 'custom.mappings'
 local userPlugins = require 'custom.plugins'
 
--- Just an example, supposed to be placed in /lua/custom/
-
 local M = {}
-
--- make sure you maintain the structure of `core/default_config.lua` here,
--- example of changing theme:
 
 M.ui = {
   theme = "one_light",
