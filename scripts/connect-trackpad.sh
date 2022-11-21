3b6fbe82f7b810cdd04af62e582aaa7e46ebab8c
Florian Merle <florian.david.merle@gmail.com>
2022-11-21 09:04:34 +0100

    fix trackpad connection script

--- scripts/connect-trackpad.sh
+++ scripts/connect-trackpad.sh
@@ -1,12 +1,18 @@
 #! /bin/bash
 
 DEVICE_NAME="Apple Inc. Magic Trackpad 2"
+PROPERTY_NAME="libinput Click Method Enabled ("
+
 DEVICE_IDS=$(xinput list --id-only "$DEVICE_NAME")
 
-for ID in $DEVICE_IDS
+for DEVICE_ID in $DEVICE_IDS
 do
-  xinput set-prop $ID 345 0, 1
+  PROPERTY_IDS=$(xinput list-props $DEVICE_ID | grep "$PROPERTY_NAME" | sed 's/.*(\(.*\)).*/\1/')
+
+  for PROPERTY_ID in $PROPERTY_IDS
+  do
+    xinput set-prop $DEVICE_ID $PROPERTY_ID 0, 1
+  done
 done
 
-notify-send -t 10000 \
-            "$DEVICE_NAME connected"
+notify-send -t 10000 "$DEVICE_NAME connected"
