0874a95c1a85463419d1b96d79b0253c968c6e6d
Florian Merle <florian.david.merle@gmail.com>
2023-04-29 13:13:35 +0200

    nvim: catppuccin latte customizations

--- nvchad/custom/chadrc.lua
+++ nvchad/custom/chadrc.lua
@@ -9,6 +9,20 @@ local highlights = require("custom.highlights")
 M.ui = {
   theme = "catppuccin_latte",
   hl_add = highlights.add,
+
+  -- see https://github.com/NvChad/nvchad.github.io/blob/03b584083b6d30e7a292b6a6e22aa9cd72988ccc/docs/config/theming.md
+  changed_themes = {
+    catppuccin_latte = {
+      base_30 = {
+        black2 = "#d9dbdf",
+      },
+      polish_hl = {
+        TelescopeSelection = {
+          bg = "#d9dbdf",
+        },
+      }
+    },
+  },
 }
 
 M.plugins = {
